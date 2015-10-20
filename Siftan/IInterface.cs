c61ef1c10d4619235cc1f7d3190d12ce56906b28
toyners <toyners@hotmail.com>
2015-10-20 21:26:31 +1100

    Moved IRecordMatchExpression to its own class.

--- Siftan/IInterface.cs
+++ Siftan/IInterface.cs
@@ -10,33 +10,5 @@ namespace Siftan
     void WriteRecord(Record record);
   }
 
-  public delegate void WriteRecordDelegate(Record record); 
-
-  public interface IRecordMatchExpression
-  {
-    Boolean IsMatch(Record record);
-
-    Boolean HasReachedMatchQuota { get; }
-
-    event WriteRecordDelegate WriteRecordToFile;
-  }
-
-  public class RecordMatchExpression
-  {
-    public RecordMatchExpression()
-    {
- 
-    }
-
-    public event WriteRecordDelegate WriteRecordToFile;
-
-    public virtual Boolean IsMatch(Record record)
-    {
-      return false;
-    }
-
-    public virtual Boolean HasReachedMatchQuota { get; protected set; }
-
-
-  }
+  public delegate void WriteRecordDelegate(Record record);
 }
---
+++ Siftan/IRecordMatchExpression.cs
@@ -0,0 +1,20 @@
+﻿
+namespace Siftan
+{
+  using System;
+
+  public interface IRecordMatchExpression
+  {
+    #region Properties
+    Boolean HasReachedMatchQuota { get; }
+    #endregion
+
+    #region Events
+    event WriteRecordDelegate WriteRecordToFile;
+    #endregion
+
+    #region Methods
+    Boolean IsMatch(Record record);
+    #endregion
+  }
+}
