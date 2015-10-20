031032f2dfae55bc8709f9492b14ae330a36d46c
toyners <toyners@hotmail.com>
2015-10-20 21:37:10 +1100

    Added in console project and Command Line Parser nuget package

---
+++ Siftan_Console/Program.cs
@@ -0,0 +1,17 @@
+﻿
+namespace Siftan_Console
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Text;
+  using System.Threading.Tasks;
+
+  public class Program
+  {
+    public static void Main(String[] args)
+    {
+
+    }
+  }
+}
