d1bd1aa03a5002b894f5581a6ab340754cb1ccfd
toyners <toyners@hotmail.com>
2016-01-12 08:42:45 +1100

    Added InListBuilder class and accompanying unit tests

---
+++ Siftan.TestSupport.UnitTests/InListBuilderTests.fs
@@ -0,0 +1,63 @@
+﻿namespace Siftan.TestSupport.UnitTests
+
+open FsUnit
+open NUnit.Framework
+open Siftan.TestSupport
+open Siftan
+
+type InListBuilderTests() = 
+
+    [<Test>]
+    member public this.``InList Builder set with value file returns correct argument array``() =
+        let fileName = @"C:\Directory\ValueFile.txt"
+        let args = InListBuilder().HasValuesFile(fileName).Build()
+
+        args.Length |> should equal 3
+        args.[0] |> should equal InListBuilder.InListKey
+        args.[1] |> should equal InListBuilder.FileKey
+        args.[2] |> should equal fileName
+
+    [<Test>]
+    member public this.``InList Builder set with value list returns correct argument array``() =
+        let values = "A:B:C"
+        let args = InListBuilder().HasValuesList(values).Build()
+
+        args.Length |> should equal 3
+        args.[0] |> should equal InListBuilder.InListKey
+        args.[1] |> should equal InListBuilder.ValuesKey
+        args.[2] |> should equal values
+
+    [<Test>]
+    member public this.``InList Builder set with default match quota returns correct argument array``() =
+        let args = InListBuilder().HasMatchQuota().Build()
+
+        args.Length |> should equal 3
+        args.[0] |> should equal InListBuilder.InListKey
+        args.[1] |> should equal InListBuilder.QuotaKey
+        args.[2] |> should equal (InListExpression.MatchQuotas.None.ToString())
+
+    [<Test>]
+    member public this.``InList Builder set with explicit match quota returns correct argument array``() =
+        let quota = InListExpression.MatchQuotas.FirstMatchInList
+        let expectedQuota = quota.ToString();
+        let args = InListBuilder().HasMatchQuota(quota).Build()
+
+        args.Length |> should equal 3
+        args.[0] |> should equal InListBuilder.InListKey
+        args.[1] |> should equal InListBuilder.QuotaKey
+        args.[2] |> should equal expectedQuota
+
+    [<Test>]
+    member public this.``InList Builder set with values and explicit match quota returns correct argument array``() =
+        let values = "A:B:C"
+        let quota = InListExpression.MatchQuotas.FirstMatchInList
+        let expectedQuota = quota.ToString();
+        let args = InListBuilder().HasValuesList(values).HasMatchQuota(quota).Build()
+
+        args.Length |> should equal 5
+        args.[0] |> should equal InListBuilder.InListKey
+        args.[1] |> should equal InListBuilder.ValuesKey
+        args.[2] |> should equal values
+        args.[3] |> should equal InListBuilder.QuotaKey
+        args.[4] |> should equal expectedQuota
+
---
+++ Siftan.TestSupport/InListBuilder.cs
@@ -0,0 +1,45 @@
+﻿
+namespace Siftan.TestSupport
+{
+  using System;
+  using System.Collections.Generic;
+
+  public class InListBuilder : IBuilder
+  {
+    public const String InListKey = "inlist";
+
+    public const String FileKey = "-f";
+
+    public const String ValuesKey = "-v";
+
+    public const String QuotaKey = "-m";
+
+    private List<String> tokens = new List<String> { InListKey };
+
+    public String[] Build()
+    {
+      return this.tokens.ToArray();
+    }
+
+    public InListBuilder HasValuesFile(String filePath)
+    {
+      this.tokens.Add(FileKey);
+      this.tokens.Add(filePath);
+      return this;
+    }
+
+    public InListBuilder HasValuesList(String list)
+    {
+      this.tokens.Add(ValuesKey);
+      this.tokens.Add(list);
+      return this;
+    }
+
+    public InListBuilder HasMatchQuota(InListExpression.MatchQuotas quota = InListExpression.MatchQuotas.None)
+    {
+      this.tokens.Add(QuotaKey);
+      this.tokens.Add(quota.ToString());
+      return this;
+    }
+  }
+}
