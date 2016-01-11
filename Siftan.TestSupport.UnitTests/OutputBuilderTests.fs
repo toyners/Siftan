5659a152c61dbb756fee3eaba4e294fc8b38dc47
toyners <toyners@hotmail.com>
2016-01-12 09:01:19 +1100

    Added OutputBuilder class and accompanying unit tests

---
+++ Siftan.TestSupport.UnitTests/OutputBuilderTests.fs
@@ -0,0 +1,44 @@
+﻿namespace Siftan.UnitTests
+
+open FsUnit
+open NUnit.Framework
+open Siftan.TestSupport
+
+type OutputBuilderTests() =
+
+    [<Test>]
+    member public this.``Output Builder set with the matched output file name returns correct argument array``() =
+        let matchedOutputFilePath = @"C:\Directory\MatchedOutputFile.txt"
+        let args = OutputBuilder().HasMatchedOutputFile(matchedOutputFilePath).Build()
+
+        args.Length |> should equal 3
+        args.[0] |> should equal OutputBuilder.OutputKey
+        args.[1] |> should equal OutputBuilder.MatchedOutputKey
+        args.[2] |> should equal matchedOutputFilePath
+
+    [<Test>]
+    member public this.``Output Builder set with the unmatched output file name returns correct argument array``() =
+        let unmatchedOutputFilePath = @"C:\Directory\UnmatchedOutputFile.txt"
+        let args = OutputBuilder().HasUnmatchedOutputFile(unmatchedOutputFilePath).Build()
+
+        args.Length |> should equal 3
+        args.[0] |> should equal OutputBuilder.OutputKey
+        args.[1] |> should equal OutputBuilder.UnmatchedOutputKey
+        args.[2] |> should equal unmatchedOutputFilePath
+
+    [<Test>]
+    member public this.``Output Builder set with both matched and unmatched output file names will return correct argument array``() =
+        let matchedOutputFilePath = @"C:\Directory\MatchedOutputFile.txt"
+        let unmatchedOutputFilePath = @"C:\Directory\UnmatchedOutputFile.txt"
+        let args = 
+            OutputBuilder()
+                .HasMatchedOutputFile(matchedOutputFilePath)
+                .HasUnmatchedOutputFile(unmatchedOutputFilePath)
+                .Build()
+
+        args.Length |> should equal 5
+        args.[0] |> should equal OutputBuilder.OutputKey
+        args.[1] |> should equal OutputBuilder.MatchedOutputKey
+        args.[2] |> should equal matchedOutputFilePath
+        args.[3] |> should equal OutputBuilder.UnmatchedOutputKey
+        args.[4] |> should equal unmatchedOutputFilePath
---
+++ Siftan.TestSupport/OutputBuilder.cs
@@ -0,0 +1,36 @@
+﻿
+namespace Siftan.TestSupport
+{
+  using System;
+  using System.Collections.Generic;
+
+  public class OutputBuilder : IBuilder
+  {
+    public const String OutputKey = "output";
+
+    public const String MatchedOutputKey = "-fm";
+
+    public const String UnmatchedOutputKey = "-fu";
+
+    private List<String> tokens = new List<String> { OutputKey };
+
+    public String[] Build()
+    {
+      return this.tokens.ToArray();
+    }
+
+    public OutputBuilder HasMatchedOutputFile(String filePath)
+    {
+      this.tokens.Add(MatchedOutputKey);
+      this.tokens.Add(filePath);
+      return this;
+    }
+
+    public OutputBuilder HasUnmatchedOutputFile(String filePath)
+    {
+      this.tokens.Add(UnmatchedOutputKey);
+      this.tokens.Add(filePath);
+      return this;
+    }
+  }
+}
