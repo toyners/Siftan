3b1d758fa2a6440d2ae19579e105c4f2fec7c5e9
toyners <toyners@hotmail.com>
2016-01-24 15:33:43 +1100

    Added IsMatching method which will perform loose log file content checking. Added unit tests to test this.

---
+++ Siftan.TestSupport.UnitTests/LogFileContentAssertionTests.fs
@@ -0,0 +1,37 @@
+﻿namespace Siftan.TestSupport.UnitTests
+
+open FsUnit
+open NUnit.Framework
+open Siftan.TestSupport
+
+type LogFileContentAssertionTests() =
+
+    [<Test>]
+    member public this.``Log file lines containing expected lines in correct orders does not throw exception``() =
+        // Assert
+        let logFileLines = [|"AAA"; "BBB"; "CCC"|]
+        let expectedLogFileLines = [|"BBB"; "CCC"|]
+
+        // Act
+        (fun () -> LogFileContentAssertion.IsMatching(logFileLines, expectedLogFileLines) |> ignore)
+        |> should not' (throw typeof<System.Exception>)
+
+    [<Test>]
+    member public this.``Log file lines contain expected lines but in wrong order throws meaningful exception``() =
+        // Assert
+        let logFileLines = [|"AAA"; "BBB"; "CCC"|]
+        let expectedLogFileLines = [|"CCC"; "BBB"|]
+        
+        // Act
+        (fun () -> LogFileContentAssertion.IsMatching(logFileLines, expectedLogFileLines) |> ignore)
+        |> should (throwWithMessage "Expected log file content lines 'BBB' to follow 'CCC' but 'CCC' follows 'BBB'.") typeof<System.Exception>
+
+    [<Test>]
+    member public this.``Log file lines does not contain expected line throws meaningful exception``() =
+        // Assert
+        let logFileLines = [|"AAA"; "BBB"; "CCC"|]
+        let expectedLogFileLines = [|"DDD"|]
+        
+        // Act
+        (fun () -> LogFileContentAssertion.IsMatching(logFileLines, expectedLogFileLines) |> ignore)
+        |> should (throwWithMessage "Missing line 'DDD' from log file content.") typeof<System.Exception>
--- Siftan.TestSupport/LogFileContentAssertion.cs
+++ Siftan.TestSupport/LogFileContentAssertion.cs
@@ -15,5 +15,47 @@ namespace Siftan.TestSupport
         Regex.IsMatch(logFileLines[index], expectedLogFileLines[index]).ShouldBeTrue();
       }
     }
+
+    public static void IsMatching(String[] logFileLines, String[] expectedLogFileLines)
+    {
+      try
+      {
+        Int32 lastMatchIndex = -1;
+        String lastMatchLine;
+
+        foreach (String expectedLogFileLine in expectedLogFileLines)
+        {
+          Boolean matched = false;
+
+          for (Int32 logIndex = 0; logIndex < logFileLines.Length; logIndex++)
+          {
+            if (Regex.IsMatch(logFileLines[logIndex], expectedLogFileLine))
+            {
+              String logFileLine = logFileLines[logIndex];
+              if (lastMatchIndex > logIndex)
+              {
+                throw new Exception(String.Format("Expected log file content lines '{0}' to follow '{1}' but '{1}' follows '{0}'.",
+                  logFileLine,
+                  logFileLines[lastMatchIndex]));
+              }
+
+              matched = true;
+              lastMatchIndex = logIndex;
+              lastMatchLine = logFileLine;
+              break;
+            }
+          }
+
+          if (!matched)
+          {
+            throw new Exception(String.Format("Missing line '{0}' from log file content.", expectedLogFileLine));
+          }
+        }
+      }
+      catch (Exception exception)
+      {
+        throw new Exception(exception.Message);
+      }
+    }
   }
 }
