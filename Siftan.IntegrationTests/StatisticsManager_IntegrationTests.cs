75d2ae73744bd02b1dc1aee14600804300415013
Benjamin Toynbee <toyners@hotmail.com>
2016-02-02 22:10:02 +1100

    Fixed tests after changing the method calls made.

--- Siftan.IntegrationTests/StatisticsManager_IntegrationTests.cs
+++ Siftan.IntegrationTests/StatisticsManager_IntegrationTests.cs
@@ -32,14 +32,14 @@ namespace Siftan.IntegrationTests
       Received.InOrder(
         () =>
         {
-          mockLogManager.WriteMessageToJobLog("2 Record(s) processed.");
-          mockLogManager.WriteMessageToJobLog("1 Record(s) matched.");
-          mockLogManager.WriteMessageToJobLog("1 Record(s) not matched.");
-          mockLogManager.WriteMessageToJobLog(String.Format("2 Record(s) processed from input file {0}.", InputFilePath));
-          mockLogManager.WriteMessageToJobLog(String.Format("1 Record(s) matched from input file {0}.", InputFilePath));
-          mockLogManager.WriteMessageToJobLog(String.Format("1 Record(s) not matched from input file {0}.", InputFilePath));
-          mockLogManager.WriteMessageToJobLog(String.Format("1 Record(s) written to output file {0}.", MatchedOutputFilePath));
-          mockLogManager.WriteMessageToJobLog(String.Format("1 Record(s) written to output file {0}.", UnmatchedOutputFilePath));
+          mockLogManager.WriteImportantMessageToJobLog("2 Record(s) processed.");
+          mockLogManager.WriteImportantMessageToJobLog("1 Record(s) matched.");
+          mockLogManager.WriteImportantMessageToJobLog("1 Record(s) not matched.");
+          mockLogManager.WriteImportantMessageToJobLog(String.Format("2 Record(s) processed from input file {0}.", InputFilePath));
+          mockLogManager.WriteImportantMessageToJobLog(String.Format("1 Record(s) matched from input file {0}.", InputFilePath));
+          mockLogManager.WriteImportantMessageToJobLog(String.Format("1 Record(s) not matched from input file {0}.", InputFilePath));
+          mockLogManager.WriteImportantMessageToJobLog(String.Format("1 Record(s) written to output file {0}.", MatchedOutputFilePath));
+          mockLogManager.WriteImportantMessageToJobLog(String.Format("1 Record(s) written to output file {0}.", UnmatchedOutputFilePath));
         });
     }
 
@@ -54,8 +54,8 @@ namespace Siftan.IntegrationTests
       statisticsManager.WriteToLog(mockLogManager);
 
       // Assert
-      mockLogManager.Received(1).WriteMessageToJobLog(Arg.Any<String>());
-      mockLogManager.Received().WriteMessageToJobLog("0 Record(s) processed.");
+      mockLogManager.Received(1).WriteImportantMessageToJobLog(Arg.Any<String>());
+      mockLogManager.Received().WriteImportantMessageToJobLog("0 Record(s) processed.");
     }
     #endregion
   }
