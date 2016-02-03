5993b237b83a081d8b7e62a636572197ccf3cad5
Benjamin Toynbee <toyners@hotmail.com>
2016-02-03 16:30:16 +1100

    Refactoring only: - Created WindowSetter that has methods for setting values on controls. - Provided methods for checking that the output files are correct (exists and content is as expected).

--- Siftan.WinForms.AcceptanceTests/DelimitedJob_WinFormAcceptanceTests.cs
+++ Siftan.WinForms.AcceptanceTests/DelimitedJob_WinFormAcceptanceTests.cs
@@ -11,7 +11,6 @@ namespace Siftan.WinForms.AcceptanceTests
   using Shouldly;
   using TestStack.White;
   using TestStack.White.UIItems;
-  using TestStack.White.UIItems.Finders;
   using TestStack.White.UIItems.WindowItems;
   using TestSupport;
 
@@ -68,55 +67,50 @@ namespace Siftan.WinForms.AcceptanceTests
       try
       {
         Window window = application.GetWindow("Siftan");
-
-        var delimiterTextBox = GetTextBoxControl(window, "Delimiter_TextBox");
-        delimiterTextBox.Text = Delimiter;
-
-        var qualifierTextBox = GetTextBoxControl(window, "Qualifier_TextBox");
-        qualifierTextBox.Text = Qualifier.ToString();
-
-        var headerLineIDTextBox = GetTextBoxControl(window, "HeaderLineID_TextBox");
-        headerLineIDTextBox.Text = HeaderLineID;
-
-        var lineIDIndex_Spinner = GetSpinnerControl(window, "LineIDIndex_Spinner");
-        lineIDIndex_Spinner.Value = LineIDIndex;
-
-        var termLineID_TextBox = GetTextBoxControl(window, "TermLineID_TextBox");
-        termLineID_TextBox.Text = TermLineID;
-
-        var termIndex_Spinner = GetSpinnerControl(window, "TermIndex_Spinner");
-        termIndex_Spinner.Value = TermIndex;
-
-        var inputDirectory_TextBox = GetTextBoxControl(window, "InputDirectory_TextBox");
-        inputDirectory_TextBox.Text = this.workingDirectory;
-
-        var inputFileName_TextBox = GetTextBoxControl(window, "InputFileName_TextBox");
-        inputFileName_TextBox.Text = this.inputFileName;
-
-        var outputDirectory_TextBox = GetTextBoxControl(window, "OutputDirectory_TextBox");
-        outputDirectory_TextBox.Text = this.workingDirectory;
-
-        var matchedOutputFileName_TextBox = GetTextBoxControl(window, "MatchedOutputFileName_TextBox");
-        matchedOutputFileName_TextBox.Text = this.matchedOutputFileName;
-
-        var createUnmatchedOutput_CheckBox = GetCheckboxControl(window, "CreateUnmatchedOutput_CheckBox");
-        createUnmatchedOutput_CheckBox.Checked = true;
-
-        var unmatchedOutputFileName_TextBox = GetTextBoxControl(window, "UnmatchedOutputFileName_TextBox");
-        unmatchedOutputFileName_TextBox.Text = this.unmatchedOutputFileName;
-
-        var inlist_TextBox = GetTextBoxControl(window, "InList_TextBox");
-        inlist_TextBox.Text = SingleValuesList;
-
-        var start_Button = GetButtonControl(window, "Start_Button");
-        start_Button.Click();
-
         var results_TextBox = window.Get<TextBox>("Results_TextBox");
+
+
+        WindowSetter windowSetter = new WindowSetter(window);
+        windowSetter
+          .SetTextBoxValue("Delimiter_TextBox", Delimiter)
+          .SetTextBoxValue("Qualifier_TextBox", Qualifier.ToString())
+          .SetTextBoxValue("HeaderLineID_TextBox", HeaderLineID)
+          .SetSpinnerValue("LineIDIndex_Spinner", LineIDIndex)
+          .SetTextBoxValue("TermLineID_TextBox", TermLineID)
+          .SetSpinnerValue("TermIndex_Spinner", TermIndex)
+          .SetTextBoxValue("InputDirectory_TextBox", this.workingDirectory)
+          .SetTextBoxValue("InputFileName_TextBox", this.inputFileName)
+          .SetTextBoxValue("OutputDirectory_TextBox", this.workingDirectory)
+          .SetTextBoxValue("MatchedOutputFileName_TextBox", this.matchedOutputFileName)
+          .SetCheckBoxChecked("CreateUnmatchedOutput_CheckBox", true)
+          .SetTextBoxValue("UnmatchedOutputFileName_TextBox", this.unmatchedOutputFileName)
+          .SetTextBoxValue("InList_TextBox", SingleValuesList)
+          .ClickButton("Start_Button");
 
         MethodRunner.RunForDuration(() => { return results_TextBox.Text.Contains("Finished."); });
 
         // Assert
-        this.Assert();
+        File.Exists(this.applicationLogFilePath).ShouldBeTrue();
+        this.AssertMatchedOutputFileIsCorrect();
+        this.AssertUnmatchedOutputFileIsCorrect();
+
+        AssertFileIsCorrect(
+          this.jobLogFilePath,
+          new String[]
+          {
+            TestConstants.DateTimeStampRegex + "Run Started...",
+            TestConstants.DateTimeStampRegex + "Record found at position 0 with Term '12345' matches with List Term '12345'.",
+            TestConstants.DateTimeStampRegex + "Record found at position 86 with Term '54321'.",
+            TestConstants.DateTimeStampRegex + Regex.Escape("2 Record(s) processed."),
+            TestConstants.DateTimeStampRegex + Regex.Escape("1 Record(s) matched."),
+            TestConstants.DateTimeStampRegex + Regex.Escape("1 Record(s) not matched."),
+            TestConstants.DateTimeStampRegex + Regex.Escape(String.Format("2 Record(s) processed from input file {0}.", this.inputFilePath)),
+            TestConstants.DateTimeStampRegex + Regex.Escape(String.Format("1 Record(s) matched from input file {0}.", this.inputFilePath)),
+            TestConstants.DateTimeStampRegex + Regex.Escape(String.Format("1 Record(s) not matched from input file {0}.", this.inputFilePath)),
+            TestConstants.DateTimeStampRegex + Regex.Escape(String.Format("1 Record(s) written to output file {0}.", this.matchedOutputFilePath)),
+            TestConstants.DateTimeStampRegex + Regex.Escape(String.Format("1 Record(s) written to output file {0}.", this.unmatchedOutputFilePath)),
+            TestConstants.DateTimeStampRegex + "Run Finished.",
+          });
       }
       finally
       {
@@ -124,12 +118,10 @@ namespace Siftan.WinForms.AcceptanceTests
       }
     }
 
-    private void Assert()
+    private void AssertMatchedOutputFileIsCorrect()
     {
-      File.Exists(this.applicationLogFilePath).ShouldBeTrue();
-      File.Exists(this.matchedOutputFilePath).ShouldBeTrue();
-      StringArrayComparison.IsMatching(
-        File.ReadAllLines(this.matchedOutputFilePath),
+      AssertFileIsCorrect(
+        this.matchedOutputFilePath,
         new String[]
         {
           "01|Ben|Toynbee|12345|1.23",
@@ -138,44 +130,18 @@ namespace Siftan.WinForms.AcceptanceTests
           "03|||12345||",
           "05|||12345||"
         });
+    }
 
-      File.Exists(this.unmatchedOutputFilePath).ShouldBeTrue();
-      StringArrayComparison.IsMatching(
-        File.ReadAllLines(this.unmatchedOutputFilePath),
+    private void AssertUnmatchedOutputFileIsCorrect()
+    {
+      AssertFileIsCorrect(
+        this.unmatchedOutputFilePath,
         new String[]
         {
           "01|Sid|Sample|54321|1.23",
           "02|||54321||",
           "03|||54321||",
           "05|||54321||"
-        });
-
-      File.Exists(this.jobLogFilePath).ShouldBeTrue();
-      StringArrayComparison.IsMatching(
-        File.ReadAllLines(this.jobLogFilePath),
-        new String[]
-        {
-          //DateTimeStampRegex + " Input Files: " + this.delimitedInputFilePath,
-          //DateTimeStampRegex + " Delimited Record Format",
-          //DateTimeStampRegex + " Delimiter: " + Delimiter,
-          //DateTimeStampRegex + " Qualifier: " + Qualifier,
-          //DateTimeStampRegex + " Line ID Index: " + LineIDIndex,
-          //DateTimeStampRegex + " Header Line ID: " + HeaderLineID,
-          //DateTimeStampRegex + " Term Line ID: " + TermLineID,
-          //DateTimeStampRegex + " Term Index: " + TermIndex,
-          //DateTimeStampRegex + " Matched Records Output File: " + this.matchedDelimitedOutputFilePath,
-          TestConstants.DateTimeStampRegex + "Run Started...",
-          TestConstants.DateTimeStampRegex + "Record found at position 0 with Term '12345' matches with List Term '12345'.",
-          TestConstants.DateTimeStampRegex + "Record found at position 86 with Term '54321'.",
-          TestConstants.DateTimeStampRegex + Regex.Escape("2 Record(s) processed."),
-          TestConstants.DateTimeStampRegex + Regex.Escape("1 Record(s) matched."),
-          TestConstants.DateTimeStampRegex + Regex.Escape("1 Record(s) not matched."),
-          TestConstants.DateTimeStampRegex + Regex.Escape(String.Format("2 Record(s) processed from input file {0}.", this.inputFilePath)),
-          TestConstants.DateTimeStampRegex + Regex.Escape(String.Format("1 Record(s) matched from input file {0}.", this.inputFilePath)),
-          TestConstants.DateTimeStampRegex + Regex.Escape(String.Format("1 Record(s) not matched from input file {0}.", this.inputFilePath)),
-          TestConstants.DateTimeStampRegex + Regex.Escape(String.Format("1 Record(s) written to output file {0}.", this.matchedOutputFilePath)),
-          TestConstants.DateTimeStampRegex + Regex.Escape(String.Format("1 Record(s) written to output file {0}.", this.unmatchedOutputFilePath)),
-          TestConstants.DateTimeStampRegex + "Run Finished.",
         });
     }
 
@@ -184,36 +150,10 @@ namespace Siftan.WinForms.AcceptanceTests
       Assembly.GetExecutingAssembly().CopyEmbeddedResourceToFile(resourceFilePath, inputFilePath);
     }
 
-    private static TextBox GetTextBoxControl(Window window, String id)
+    private static void AssertFileIsCorrect(String filePath, String[] expectedFileLines)
     {
-      return (TextBox)GetControl(window, typeof(TextBox), id);
-    }
-
-    private static Spinner GetSpinnerControl(Window window, String id)
-    {
-      return (Spinner)GetControl(window, typeof(Spinner), id);
-    }
-
-    private static Button GetButtonControl(Window window, String id)
-    {
-      return (Button)GetControl(window, typeof(Button), id);
-    }
-
-    private static CheckBox GetCheckboxControl(Window window, String id)
-    {
-      return (CheckBox)GetControl(window, typeof(CheckBox), id);
-    }
-
-    private static IUIItem GetControl(Window window, Type controlType, String id)
-    {
-      SearchCriteria searchCriteria =
-        SearchCriteria.
-          ByAutomationId(id).
-          AndControlType(controlType, WindowsFramework.WinForms);
-
-      var control = window.Get(searchCriteria);
-      window.WaitWhileBusy();
-      return control;
+      File.Exists(filePath).ShouldBeTrue(String.Format("File '{0}' does not exist", filePath));
+      StringArrayComparison.IsMatching(File.ReadAllLines(filePath), expectedFileLines);
     }
   }
 }
---
+++ Siftan.WinForms.AcceptanceTests/WindowSetter.cs
@@ -0,0 +1,76 @@
+﻿
+namespace Siftan.WinForms.AcceptanceTests
+{
+  using System;
+  using Jabberwocky.Toolkit.Object;
+  using TestStack.White.UIItems;
+  using TestStack.White.UIItems.WindowItems;
+
+  /// <summary>
+  /// Used to set the values of the controls in a Teststack.White window.
+  /// </summary>
+  public class WindowSetter
+  {
+    private readonly Window window;
+
+    /// <summary>
+    /// Initializes a new instance of the WindowSetter class.
+    /// </summary>
+    /// <param name="window">Reference to the window containing the controls to set.</param>
+    public WindowSetter(Window window)
+    {
+      window.VerifyThatObjectIsNotNull("Parameter 'window' is null.");
+      this.window = window;
+    }
+
+    /// <summary>
+    /// Sets the value of the text box control.
+    /// </summary>
+    /// <param name="id">Id of the text box control.</param>
+    /// <param name="value">Value to set in the text box.</param>
+    /// <returns>Reference to allow chaining.</returns>
+    public WindowSetter SetTextBoxValue(String id, String value)
+    {
+      var textBox = this.window.Get<TextBox>(id);
+      this.window.WaitWhileBusy();
+      textBox.Text = value;
+      return this;
+    }
+
+    /// <summary>
+    /// Sets the value of the spinner control.
+    /// </summary>
+    /// <param name="id">Id of the spinner control.</param>
+    /// <param name="value">Value to set in the spinner control.</param>
+    /// <returns>Reference to allow chaining.</returns>
+    public WindowSetter SetSpinnerValue(String id, Double value)
+    {
+      var spinner = this.window.Get<Spinner>(id);
+      this.window.WaitWhileBusy();
+      spinner.Value = value;
+      return this;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="id">Id of the check box.</param>
+    /// <param name="value">True to check the control box; false otherwise.</param>
+    /// <returns>Reference to allow chaining.</returns>
+    public WindowSetter SetCheckBoxChecked(String id, Boolean value)
+    {
+      var checkBox = this.window.Get<CheckBox>(id);
+      this.window.WaitWhileBusy();
+      checkBox.Checked = value;
+      return this;
+    }
+
+    public WindowSetter ClickButton(String id)
+    {
+      var button = this.window.Get<Button>(id);
+      this.window.WaitWhileBusy();
+      button.Click();
+      return this;
+    }
+  }
+}
