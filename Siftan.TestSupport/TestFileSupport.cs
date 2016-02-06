ad17407f136271b09b969162b439c37350871a39
Benjamin Toynbee <toyners@hotmail.com>
2016-02-06 18:25:11 +1100

    Added new static class which provides support methods for files created or needed by tests.

---
+++ Siftan.TestSupport/TestFileSupport.cs
@@ -0,0 +1,26 @@
+﻿
+namespace Siftan.TestSupport
+{
+  using System;
+  using System.IO;
+  using System.Reflection;
+  using Jabberwocky.Toolkit.Assembly;
+  using Shouldly;
+
+  /// <summary>
+  /// Provides support methods for files created or needed by tests.
+  /// </summary>
+  public static class TestFileSupport
+  {
+    public static void CreateFile(String resourceFilePath, String inputFilePath)
+    {
+      Assembly.GetExecutingAssembly().CopyEmbeddedResourceToFile(resourceFilePath, inputFilePath);
+    }
+
+    public static void AssertFileIsCorrect(String filePath, String[] expectedFileLines)
+    {
+      File.Exists(filePath).ShouldBeTrue(String.Format("File '{0}' does not exist", filePath));
+      StringArrayComparison.IsMatching(File.ReadAllLines(filePath), expectedFileLines);
+    }
+  }
+}
