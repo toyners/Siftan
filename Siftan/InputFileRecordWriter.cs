02921585103c3c8073d8ef42f2c5a9be5063c8ad
Benjamin Toynbee <toyners@hotmail.com>
2016-02-24 18:50:30 +1100

    Added InputFileRecordWriter class. Writes out the record to a output file based on the input file it came from.

---
+++ Siftan/InputFileRecordWriter.cs
@@ -0,0 +1,40 @@
+﻿
+namespace Siftan
+{
+  using System;
+  using Jabberwocky.Toolkit.IO;
+
+  public class InputFileRecordWriter : IRecordWriter
+  {
+    public Boolean DoWriteMatchedRecords
+    {
+      get
+      {
+        throw new NotImplementedException();
+      }
+    }
+
+    public Boolean DoWriteUnmatchedRecords
+    {
+      get
+      {
+        throw new NotImplementedException();
+      }
+    }
+
+    public void Close()
+    {
+      throw new NotImplementedException();
+    }
+
+    public void WriteMatchedRecord(IStreamReader reader, Record record)
+    {
+      throw new NotImplementedException();
+    }
+
+    public void WriteUnmatchedRecord(IStreamReader reader, Record record)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
