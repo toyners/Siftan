98edc0b447b98fbd45c23806babbe1dbdb421144
Benjamin Toynbee <toyners@hotmail.com>
2016-03-09 21:38:15 +1100

    Added integration test for InputFileRecordWriter. Implemented WriteMatchedRecord and Close methods.

---
+++ Siftan.IntegrationTests/InputFileRecordWriter_IntegrationTests.cs
@@ -0,0 +1,42 @@
+﻿
+namespace Siftan.IntegrationTests
+{
+  using System;
+  using System.IO;
+  using FluentAssertions;
+  using Jabberwocky.Toolkit.IO;
+  using NSubstitute;
+  using NUnit.Framework;
+  using Shouldly;
+
+  [TestFixture]
+  public class InputFileRecordWriter_IntegrationTests
+  {
+    [Test]
+    public void RecordsFromMultipleInputFilesWrittenToCorrespondingOutputFiles()
+    {
+      // Arrange
+      File.WriteAllLines("FileA.txt", new String[] { "a", "b", "c", "d", "e", "f" });
+      File.WriteAllLines("FileB.txt", new String[] { "g", "h", "i", "j", "k", "l" });
+
+      var mockStatisticsCollector = Substitute.For<IStatisticsCollector>();
+      InputFileRecordWriter writer = new InputFileRecordWriter(mockStatisticsCollector);
+      FileReader inputFileA = new FileReader("FileA.txt");
+      FileReader inputFileB = new FileReader("FileB.txt");
+
+      // Act
+      writer.WriteMatchedRecord(inputFileA, new Record { Start = 0, End = 8 });
+      writer.WriteMatchedRecord(inputFileB, new Record { Start = 0, End = 8 });
+      writer.WriteMatchedRecord(inputFileA, new Record { Start = 9, End = 18 });
+      writer.WriteMatchedRecord(inputFileB, new Record { Start = 9, End = 18 });
+      writer.Close();
+
+      // Assert
+      File.Exists("Matched_From_FileA.txt").ShouldBeTrue();
+      File.ReadAllLines("Matched_From_FileA.txt").ShouldBeEquivalentTo(new String[] { "a", "b", "c", "d", "e", "f" });
+
+      File.Exists("Matched_From_FileB.txt").ShouldBeTrue();
+      File.ReadAllLines("Matched_From_FileB.txt").ShouldBeEquivalentTo(new String[] { "g", "h", "i", "j", "k", "l" });
+    }
+  }
+}
--- Siftan/Record Writers/InputFileRecordWriter.cs
+++ Siftan/Record Writers/InputFileRecordWriter.cs
@@ -2,11 +2,15 @@
 namespace Siftan
 {
   using System;
+  using System.Collections.Generic;
+  using System.IO;
   using Jabberwocky.Toolkit.IO;
 
   public class InputFileRecordWriter : IRecordWriter
   {
     private IStatisticsCollector statisticsCollector;
+
+    private Dictionary<String, StreamWriter> writers = new Dictionary<String, StreamWriter>();
 
     public InputFileRecordWriter(IStatisticsCollector statisticsCollector)
     {
@@ -31,12 +35,27 @@ namespace Siftan
 
     public void Close()
     {
-      throw new NotImplementedException();
+      foreach (var writer in this.writers.Values)
+      {
+        writer.Close();
+      }
     }
 
     public void WriteMatchedRecord(IStreamReader reader, Record record)
     {
-      throw new NotImplementedException();
+      StreamWriter writer;
+      if (!writers.ContainsKey(reader.Name))
+      {
+        String outputName = Path.GetDirectoryName(reader.Name) + "Matched_From_" + Path.GetFileName(reader.Name);
+        writer = new StreamWriter(outputName);
+        writers.Add(reader.Name, writer);
+      }
+      else
+      {
+        writer = writers[reader.Name];
+      }
+
+      StreamWriteOperations.WriteRecordToStream(writer, reader, record);
     }
 
     public void WriteUnmatchedRecord(IStreamReader reader, Record record)
