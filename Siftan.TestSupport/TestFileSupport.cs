4b8b9eaad083a8d0ab5de0b08abf60049fd4aebf
Benjamin Toynbee <toyners@hotmail.com>
2016-03-15 17:12:48 +1100

    Added method comments.

--- Siftan.TestSupport/TestFileSupport.cs
+++ Siftan.TestSupport/TestFileSupport.cs
@@ -10,6 +10,11 @@ namespace Siftan.TestSupport
   /// </summary>
   public static class TestFileSupport
   {
+    /// <summary>
+    /// Check that the file exists and that it contains the expected file lines.
+    /// </summary>
+    /// <param name="filePath">Full path of file to check.</param>
+    /// <param name="expectedFileLines">Lines expected to be found in file.</param>
     public static void AssertFileIsCorrect(String filePath, String[] expectedFileLines)
     {
       File.Exists(filePath).ShouldBeTrue(String.Format("File '{0}' does not exist", filePath));
