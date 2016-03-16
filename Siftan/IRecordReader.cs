51b309e52bdb9ee52b8c779e74c710d8843cabe7
Benjamin Toynbee <toyners@hotmail.com>
2016-03-16 16:51:55 +1100

    Added IRecordSource which will supersede the IRecordReader interface.

--- Siftan/IRecordReader.cs
+++ Siftan/IRecordReader.cs
@@ -1,6 +1,7 @@
 ﻿
 namespace Siftan
 {
+  using System;
   using Jabberwocky.Toolkit.IO;
 
   public interface IRecordReader
@@ -8,5 +9,16 @@ namespace Siftan
     #region Methods
     Record ReadRecord(IStreamReader reader);
     #endregion
+  }
+
+  public interface IRecordSource
+  {
+    Int64 GetRecordCount { get; }
+
+    Boolean MoveToNextRecord();
+
+    Boolean MoveToRecord(Int64 index);
+
+    Boolean GetRecordData(Byte[] buffer, out Int64 bytesRead);
   }
 }
