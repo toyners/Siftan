23ba8d8b5685031baa3add4efb2e969364d062b8
Benjamin Toynbee <toyners@hotmail.com>
2016-03-16 17:14:12 +1100

    Added empty integration test class for use with DelimitedRecordSource class.

---
+++ Siftan.IntegrationTests/DelimitedRecordSource_IntegrationTests.cs
@@ -0,0 +1,29 @@
+﻿
+namespace Siftan.IntegrationTests
+{
+  using System;
+  using NSubstitute;
+  using NUnit.Framework;
+  using Shouldly;
+
+  [TestFixture]
+  public class DelimitedRecordSource_IntegrationTests
+  {
+    #region Methods
+    [TestFixtureSetUp]
+    public void SetupBeforeAllTests()
+    {
+    }
+
+    [SetUp]
+    public void SetupBeforeEachTest()
+    {
+    }
+
+    [Test]
+    public void Test()
+    {
+    }
+    #endregion 
+  }
+}
